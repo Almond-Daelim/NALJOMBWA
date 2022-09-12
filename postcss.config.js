3d571d95176366787dfe1fdc8fca1ee2ebef84e7
ChoiSunAh, 최선아 <csa2676@naver.com>
2022-09-12 17:02:16 +0900

    tailwind setting

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
-/* eslint-disable prettier/prettier */
-const tailwindcss = require('tailwindcss');
 module.exports = {
-    plugins: [tailwindcss('./tailwind.js'), require('autoprefixer')],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+import './tailwind.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="text-3xl font-bold underline">Hello world!</div>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+      </button>
+    </>
   );
 }
 
