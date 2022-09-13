6d6c25a67b31d2793b1e9ce5aa9ba1d0dd261519
ChoiSunAh, 최선아 <csa2676@naver.com>
2022-09-13 18:51:38 +0900

    init setting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import '../src/styles/global.css';
+import { Mobile, PC } from '../src/hooks/useMediaQuery';
 
 function App() {
   return (
     <>
+      <div>
+        <Mobile>
+          <div className="mobile_container">이건 모바일 !!</div>
+        </Mobile>
+        <div className="pc_container">
+          <PC>요건 PC !!!</PC>
+        </div>
+      </div>
       <div className="text-3xl font-bold underline">Hello world!</div>
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Button
---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { useMediaQuery } from 'react-responsive';
+
+const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({
+        query: '(max-width:767px)',
+    });
+    return <React.Fragment>{isMobile && children}</React.Fragment>;
+};
+
+const PC = ({ children }) => {
+    const isPc = useMediaQuery({
+        query: '(min-width:768px) ',
+    });
+    return <React.Fragment>{isPc && children}</React.Fragment>;
+};
+
+export { Mobile, PC };
