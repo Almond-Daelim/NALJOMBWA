4bdb76292a4d37daff2e02cc24bf46648e95c927
ChoiSunAh, 최선아 <csa2676@naver.com>
2022-09-13 19:21:44 +0900

    init setting

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="m-5 text-blue-500 font-bold">Hello Home</h1>
+    </div>
+  );
+};
+
+export default Home;
