41bb187937730b938077aaadcab8e4c9b0ea8dbf
hsbin316 <hsbin316@gmail.com>
2022-09-15 23:05:46 +0900

    feat: Footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Footer from '../components/common/Layout/Footer';
 
 const Home = () => {
   return (
     <div>
       <h1 className="m-5 text-blue-500 font-bold">Hello Home</h1>
+      <Footer></Footer>
     </div>
   );
 };
