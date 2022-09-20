dbe07cda5c4ddb003624f3875a3d5c0945810f12
ChoiSunAh, 최선아 <csa2676@naver.com>
2022-09-20 18:47:54 +0900

    update dev

---
+++ src/components/common/Layout/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
+      <div>
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          Copyright &copy; {new Date().getFullYear()} Team - Almond
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
