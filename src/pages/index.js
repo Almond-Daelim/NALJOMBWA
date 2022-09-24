af4b15192dfc374631df209025d1123bea2723a6
hsbin316 <hsbin316@gmail.com>
2022-09-24 21:08:17 +0900

    feat:darkmode

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Switcher from '../components/common/darkmode/switcher'
 
 const Home = () => {
   return (
     <div>
-      <h1 className="m-5 text-blue-500 font-bold">Hello Home</h1>
+      <Switcher />
+      <h1 className="m-5 text-blue-500 font-bold dark:bg-gray-900 ">Hello Home</h1>
     </div>
   );
 };
