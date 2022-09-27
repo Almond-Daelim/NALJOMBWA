3e14aca4d7cfcba3b82c28915e8ed1aa33894f95
hsbin316 <hsbin316@gmail.com>
2022-09-27 22:17:49 +0900

    feat:DarkMode

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/common/Layout/Header';
 import Footer from '../components/common/Layout/Footer';
+import Switcher from '../components/common/DarkMode/Switcher';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Switcher />
       <Footer />
     </div>
   );
