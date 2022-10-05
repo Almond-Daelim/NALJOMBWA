f7cb3d2e0d0c1e5ff5a06d0d0f722bb15a351604
CSN-ah22 <csa2676@naver.com>
2022-10-05 17:03:11 +0900

    feat: 검색 기능 테스트

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 
 const MainPage = () => {
   const [data, setData] = useState([]);
+  const kewWord = '억새';
+
   useEffect(() => {
     (async () => {
       const response = await fetch(
@@ -13,6 +15,26 @@ const MainPage = () => {
       setData(json.response.body.items.item);
     })();
   }, []);
+
+  const searchTest = data
+    .filter(data => 0 === data.title.indexOf(kewWord))
+    .map((post, idx) => {
+      return (
+        <div key={idx}>
+          <ul>
+            <ul className=" bg-slate-400 w-full  h-[304px] rounded-[10px]">
+              <img
+                className="w-[100%] h-[100%] object-cover rounded-[10px]"
+                src={post.firstimage}
+                alt={post.title + '이미지'}
+              />
+            </ul>
+            <li>{post.title}</li>
+            <li>{post.addr1}</li>
+          </ul>
+        </div>
+      );
+    });
 
   const listItems = data
     .filter(data => data.firstimage && data)
@@ -36,7 +58,7 @@ const MainPage = () => {
 
   return (
     <div className="grid justify-center grid-cols-auto-fit gap-x-[34px] gap-y-[82px]">
-      {listItems}
+      {searchTest}
     </div>
   );
 };
