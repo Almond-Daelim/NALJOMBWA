357d2150a9f03261685f445bb8ede26cf5937e8c
CSN-ah22 <csa2676@naver.com>
2022-10-19 16:25:07 +0900

    feat: search api test

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const MainPage = () => {
   const [data, setData] = useState([]);
+  const [apiList, setApiList] = useState('/areaBasedList');
+  const keywWord = '안양';
+  const kw_result = encodeURIComponent(keywWord);
+  const api = {
+    key: 'jE3ugMXmJI0yag2gClOOzYsaGhuRCsAy6yRix3i4CzyIKUAEbYeBEwTzAmr847RBStVkvt6rkKxqa8ziDhOqlQ%3D%3D',
+    base: 'https://apis.data.go.kr/B551011/KorService',
+    MobileOS: 'ETC',
+    MoblieApp: 'NALJOMBWA',
+    listYN: 'Y',
+    arrange: 'C',
+    numOfRows: '20',
+    pageNo: '1',
+    _type: 'json',
+  };
+  const url = `${api.base}/searchKeyword?serviceKey=${api.key}&numOfRows=${api.numOfRows}&pageNo=${api.pageNo}&MobileOS=${api.MobileOS}&MobileApp=${api.MoblieApp}&_type=${api._type}&listYN=${api.listYN}&arrange=${api.arrange}&keyword=${kw_result}`;
   useEffect(() => {
     (async () => {
-      const response = await fetch(
-        `https://apis.data.go.kr/B551011/KorService/areaBasedList?serviceKey=jE3ugMXmJI0yag2gClOOzYsaGhuRCsAy6yRix3i4CzyIKUAEbYeBEwTzAmr847RBStVkvt6rkKxqa8ziDhOqlQ%3D%3D&numOfRows=20&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=C`,
-      );
+      const response = await fetch(url);
 
       const json = await response.json();
 
@@ -14,15 +28,26 @@ const MainPage = () => {
     })();
   }, []);
 
+  // if (keywWord && keywWord) {
+  //   const kw_result = encodeURIComponent(keywWord);
+  //   setApiList('/searchKeyword');
+  //   (async () => {
+  //     const res = await fetch(url + `&keyword=${kw_result}`);
+  //     console.log(url + `&keyword=${kw_result}`);
+  //     const json = await res.json();
+  //     setData(json.response.body.items.item);
+  //   })();
+  // }
+
   const listItems = data
     .filter(data => data.firstimage && data)
     .map((post, idx) => {
       return (
         <div key={idx}>
           <ul>
-            <ul className=" bg-slate-400 w-full  h-[304px] rounded-[10px]">
+            <ul class=" bg-slate-400 w-full  h-[304px] rounded-[10px]">
               <img
-                className="w-[100%] h-[100%] object-cover rounded-[10px]"
+                class="w-[100%] h-[100%] object-cover rounded-[10px]"
                 src={post.firstimage}
                 alt={post.title + '이미지'}
               />
