398a2a22c03b38f5d01a630278018bb1ba053b7a
hsbin316 <hsbin316@gmail.com>
2022-10-26 21:19:55 +0900

    feat: weather-api

---
+++ src/Api/Weather/Datas/WeatherData.js
@@ -0,0 +1,21 @@
+const WeatherData = [
+  { name: 'Seoul', city_id: '1835847', kr: '서울' },
+  { name: 'Busan', city_id: '1838524', kr: '부산' },
+  { name: 'Daegu', city_id: '1835329', kr: '대구' },
+  { name: 'Incheon', city_id: '1843564', kr: '인천' },
+  { name: 'Gwangju', city_id: '1841811', kr: '광주' },
+  { name: 'Daejeon', city_id: '1835235', kr: '대전' },
+  { name: 'Ulsan', city_id: '1833747', kr: '울산' },
+  { name: 'Sejong', city_id: '1835235', kr: '세종' },
+  { name: 'Gyeonggi', city_id: '1841610', kr: '경기' },
+  { name: 'Gangwon', city_id: '1843125', kr: '강원' },
+  { name: 'Chungcheongbuk', city_id: '1845106', kr: '충북' },
+  { name: 'Chungcheongnam', city_id: '1845105', kr: '충남' },
+  { name: 'Jeollabuk', city_id: '1845789', kr: '전북' },
+  { name: 'Jeollanam', city_id: '1845788', kr: '전남' },
+  { name: 'Gyeongsangbuk', city_id: '1841597', kr: '경북' },
+  { name: 'Gyeongsangnam  ', city_id: '1902028', kr: '경남' },
+  { name: 'Jeju', city_id: '1846266', kr: '제주' },
+];
+
+export default WeatherData;
