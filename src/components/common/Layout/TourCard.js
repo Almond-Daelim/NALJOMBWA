bd216fc7c080268f011bc99ee06c0af91a1bfebb
ChoiSunAh, 최선아 <csa2676@naver.com>
2022-11-08 02:38:36 +0900

    feat: detaile-Info 이름과 주소 출력 완료 기본적인 css 레이아웃 구성 완료

--- src/components/common/Layout/TourCard.js
+++ src/components/common/Layout/TourCard.js
@@ -1,19 +1,30 @@
+import { Link } from 'react-router-dom';
 const TourCard = api_data => {
   const tourData = api_data;
-  const { firstimage, title, addr1 } = tourData.data;
+  const { firstimage, title, addr1, firstimage2 } = tourData.data;
   return (
     <div className="grid justify-center grid-cols-auto-fit gap-x-[34px] gap-y-[82px]">
-      <ul>
-        <ul class=" bg-slate-400 w-full  h-[304px] rounded-[10px]">
-          <img
-            class="w-[100%] h-[100%] object-cover rounded-[10px]"
-            src={firstimage}
-            alt={title + '이미지'}
-          />
+      <Link
+        to={`/detail/${title}`}
+        state={{
+          firstimage: firstimage,
+          firstimage2: firstimage2,
+          title: title,
+          addr1: addr1,
+        }}
+      >
+        <ul>
+          <ul className=" bg-slate-400 w-full  h-[304px] rounded-[10px]">
+            <img
+              className="w-[100%] h-[100%] object-cover rounded-[10px]"
+              src={firstimage}
+              alt={title + '이미지'}
+            />
+          </ul>
+          <li>{title}</li>
+          <li>{addr1}</li>
         </ul>
-        <li>{title}</li>
-        <li>{addr1}</li>
-      </ul>
+      </Link>
     </div>
   );
 };
--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,5 +1,56 @@
-const detail = props => {
-  console.log('test');
-  return <>디테일 페이지 입니다.</>;
+import { useLocation } from 'react-router-dom';
+import Header from '../components/common/Layout/Header';
+const Detail = () => {
+  const location = useLocation();
+  const title = location.state.title;
+  const firstimage = location.state.firstimage;
+  const addr1 = location.state.addr1;
+  return (
+    <>
+      <Header />
+      <div className="flex justify-center min-w-[730.25px]">
+        <div className="flex-wrap justify-center row-auto">
+          <div className="flex justify-center w-[730.25px] h-[730.75px] bg-slate-500 rounded-[15px]">
+            <img
+              className="w-[100%] object-cover rounded-[15px]"
+              src={firstimage}
+              alt={title + '이미지'}
+            />
+          </div>
+          <div className="flex w-[730.25px] ">
+            <ul>
+              <li>{title}</li>
+              <li>{addr1}</li>
+            </ul>
+          </div>
+          <div className="flex w-[732.25px] h-[54px] bg-slate-200">
+            <div className="flex justify-center w-[243px] h-[54px] bg-slate-300">
+              <ul className="flex items-center">
+                <li>기본정보</li>
+              </ul>
+            </div>
+            <div className="flex justify-center w-[243px] h-[54px] bg-slate-300">
+              <ul className="flex items-center">
+                <li>소개</li>
+              </ul>
+            </div>
+            <div className="flex justify-center w-[243px] h-[54px] bg-slate-300">
+              <ul className="flex items-center">
+                <li>선호도</li>
+              </ul>
+            </div>
+          </div>
+          <div className="flex-wrap w-[730.25px] h-[508px] bg-slate-200">
+            <div className=" w-[243px] h-[74px] bg-lime-100">주소</div>
+            <div className="  w-[243px] h-[74px] bg-lime-300">개장일</div>
+            <div className="  w-[243px] h-[74px] bg-lime-500">쉬는날</div>
+            <div className="  w-[243px] h-[74px] bg-lime-700">이용시간</div>
+            <div className="  w-[243px] h-[74px] bg-teal-50">주차시설</div>
+            <div className="  w-[243px] h-[74px] bg-teal-200">홈페이지</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
-export default detail;
+export default Detail;
