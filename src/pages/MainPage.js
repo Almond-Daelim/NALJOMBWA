97815f6ab477fc88987c7bde00bdc8383e77ccd8
hsbin316 <hsbin316@gamil.com>
2022-11-09 15:18:42 +0900

    fix: move-weather-data

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../components/common/Button/Button';
 import ArrowButton from '../components/common/Button/ArrowButton/ArrowButton';
 import Switcher from '../components/common/DarkMode/Switcher';
 import Weather from '../Api/Weather/Weather';
-import WeatherMap from '../Api/Weather/WeatherMap';
 import TourListPage from './TourListPage';
 
 const MainPage = () => {
+  const [cityName, setCityName] = useState(null);
   const handleClickRight = () => {
     alert('right');
   };
@@ -15,11 +15,19 @@ const MainPage = () => {
   };
   const moveCityData = x => {
     console.log(x);
+    setCityName(x);
   };
   return (
     <>
-      <WeatherMap />
-      <Weather moveCityData={moveCityData} />
+      <div class="max-w-2xl py-4 pl-7 bg-emerald-200">
+        <Weather moveCityData={moveCityData} />
+        <h3>오늘의 관광지 추천!</h3>
+        <div>{cityName}</div>
+        <Button
+          title={'의 추천 관광지 \n 리스트 보러 가기 →'}
+          onPress={() => alert('asdf')}
+        />
+      </div>
       <Switcher />
       <ArrowButton
         direction="left"
@@ -29,11 +37,14 @@ const MainPage = () => {
         direction="right"
         onPress={() => handleClickRight()}
       />
-      <Button
-        title={'가평의 추천 관광지 \n 리스트 보러 가기 →'}
-        onPress={() => alert('asdf')}
-      />
-      <TourListPage pageNo={0} />
+      {cityName ? (
+        <TourListPage
+          pageNo={1}
+          keyWord={cityName}
+        />
+      ) : (
+        <div>정보없음</div>
+      )}
     </>
   );
 };
